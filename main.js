85d9c576778e653065bbb95836059c0d90888177
Vineet <vineetpandit@gmail.com>
2015-01-17 18:05:25 -0800

    Added map link and button

--- main.js
+++ main.js
@@ -40,4 +40,10 @@ $(document).ready(function () {
             });
         }
     });
+
+    $(".landing-next-button").on("click", function () {
+        $('html,body').animate({
+            scrollTop: $(".story-page").offset().top
+        });
+    });
 });
