a5782d7ebbdd94aef75eadd4ee16fa92ed2dccb5
vineet-pandit <vineetpandit@gmail.com>
2015-01-27 20:41:00 -0800

    Added links

--- main.js
+++ main.js
@@ -2,10 +2,18 @@ $(document).ready(function () {
     $(".landing-next-button").on("click", function () {
         $('html,body').animate({
             scrollTop: $(".story-page").offset().top
-        });
+        }, 1200, 'easeInOutExpo');
+        $('body').removeClass('no-scroll');
     });
 });
 
 $(window).load(function () {
-
+    var landingFlowers = $(".landing-flowers");
+    landingFlowers.fadeIn(1000);
+    landingFlowers.removeAttr('style');
+    landingFlowers.css("display", 'flex');
+    landingFlowers.css("display", '-webkit-flex');
+    $(".landing-title").fadeIn(2000, 'easeInExpo');
+    $(".landing-subtitle").fadeIn(3000, 'easeInExpo');
+    $(".landing-next-button-container").fadeIn(4000, 'easeInExpo');
 });
