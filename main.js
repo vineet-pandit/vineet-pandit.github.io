49ede56e84fabe59d193977473972fdcfcf99d66
vineet-pandit <vineetpandit@gmail.com>
2015-01-28 22:47:29 -0800

    done donadone done

--- main.js
+++ main.js
@@ -8,10 +8,15 @@ $(document).ready(function () {
 
 $(window).load(function () {
     var landingFlowers = $(".landing-flowers");
-    landingFlowers.fadeIn(1000);
-    landingFlowers.addClass('flex-display');
-    landingFlowers.removeAttr('style');
-    $(".landing-title").fadeIn(2000, 'easeInExpo');
-    $(".landing-subtitle").fadeIn(3000, 'easeInExpo');
-    $(".landing-next-button-container").fadeIn(4000, 'easeInExpo');
+    landingFlowers.fadeIn(1000, 'easeInExpo', function() {
+        landingFlowers.addClass('flex-display');
+        landingFlowers.removeAttr('style');
+        $(".landing-title").fadeIn(1000, 'easeInExpo');
+        $(".landing-subtitle").fadeIn(2000, 'easeInExpo');
+        $(".landing-next-button-container").fadeIn(3000, 'easeInExpo');
+    });
+});
+
+$(window).on('beforeunload', function() {
+    $(window).scrollTop(0);
 });
